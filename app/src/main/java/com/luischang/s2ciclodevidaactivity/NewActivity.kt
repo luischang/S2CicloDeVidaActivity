16f2ba5838e572dd91740473aa7df76e3f55a967
Luis Chang <luis.chang@outlook.com>
2023-04-19 19:59:14 -0500

    Se agregó una pantalla New Activity con un texto de hola mundo

---
+++ app/src/main/java/com/luischang/s2ciclodevidaactivity/NewActivity.kt
@@ -0,0 +1,11 @@
+package com.luischang.s2ciclodevidaactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new)
+    }
+}
